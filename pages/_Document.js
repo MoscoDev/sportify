b45d824aade552f3e02ce8d1caf4d628e7052bf6
Audu Moses <89123661+MoscoDev@users.noreply.github.com>
2022-06-27 21:14:11 +0100

    manifest.json bug fixed

--- pages/_Document.js
+++ pages/_Document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/logo-192x192.png"></link>
+          <link rel="apple-touch-icon" href="./public/logo-192x192.png"></link>
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
